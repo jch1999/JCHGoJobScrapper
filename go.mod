1d59abf7d8e14607187aefebe99457814c61ba54
jch1999 <54787512+jch1999@users.noreply.github.com>
2023-08-03 05:57:05 +0000

    JCH go web scrapper until nomad 4.6

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.19
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/tsak/concurrent-csv-writer v1.0.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	// "encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	// "os"
+	"github.com/tsak/concurrent-csv-writer"
 	"strconv"
 	"strings"
 
@@ -14,26 +17,36 @@ type extractedJob struct {
 	id       string
 	title    string
 	location string
-	// salary   string // salary가 있는 것도 없는 것도 존재. 위치가 비정규적
+	// salary   string // salary가 있는 것도 없는 것도 존재.
 }
 
-//3:30
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
 func main() {
 	var jobs []extractedJob
 	totalPages := getPages()
-	// fmt.Println(totalPages)
+
+	mainChannel := make(chan []extractedJob)
 	for i := 0; i < totalPages; i++ {
-		extractedJobs := getPage(i)
+		// extractedJobs := getPage(i)
+		go getPage(i, mainChannel)
 		// extractedJobs... 는 extractedJobs의 content를 가져온다는 의미?
-    //slice 크기가 정해지지 않은 배열
+		//배열을 추가하는게 아닌 배열의 내용물을 추가하기 위해 배열... 사용
+		// jobs = append(jobs, extractedJobs...)
+	}
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := <-mainChannel
 		jobs = append(jobs, extractedJobs...)
 	}
-	fmt.Println(jobs)
+	// fmt.Println(jobs)
+	// writeJobs(jobs)
+	writeJobsRoutine(jobs)
+	fmt.Println("Done, extracted ", len(jobs))
 }
-func getPage(page int) []extractedJob {
+
+func getPage(page int, mainChannel chan []extractedJob) {
 	var jobs []extractedJob
+	c := make(chan extractedJob)
 	pageURL := baseURL + "&recruitPage=" + strconv.Itoa((page))
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
@@ -44,11 +57,17 @@ func getPage(page int) []extractedJob {
 	checkErr(err)
 	searchCards := doc.Find(".item_recruit")
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		job := extractJob(card)
-		jobs = append(jobs, job)
+		// job := extractJob(card, c)
+		// jobs = append(jobs, job)
+		go extractJob(card, c)
 	})
-	return jobs
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+	mainChannel <- jobs
 }
+
 func getPages() int {
 	pages := 0
 	res, err := http.Get(baseURL)
@@ -61,7 +80,6 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	// fmt.Println(doc)
 	//.pagintion이 하나라서 Each를 써도 문제가 없는 건가보다
 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
 		// fmt.Println(s.Find("a").Length())
@@ -70,15 +88,15 @@ func getPages() int {
 	return pages
 }
 
-func extractJob(card *goquery.Selection) extractedJob {
+func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 	id, _ := card.Attr("value")
-	// fmt.Println(id)
 	title := cleanString(card.Find(".area_job>.job_tit>a>span").Text())
-	// fmt.Println(title)
 	location := cleanString(card.Find(".area_job>.job_condition>span>a").Text())
-	// fmt.Println(location)
-	// fmt.Println(id, title, location)
-	return extractedJob{id: id,
+
+	// return extractedJob{id: id,
+	// 	title:    title,
+	// 	location: location}
+	c <- extractedJob{id: id,
 		title:    title,
 		location: location}
 }
@@ -87,9 +105,57 @@ func cleanString(str string) string {
 	//Fields는 문자열을 분리시킨다.
 	//stringTokenizer와 같은 것?
 	//TrimSpace로 양쪽 끝에서의 공백을 제거
-	//Join은 배열을 separater을 상요해 합친다.
+	//Join은 배열을 separater을 사용해 합친다.
 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
+
+func writeJobsRoutine(jobs []extractedJob) {
+	file, err := ccsv.NewCsvWriter("jobs.csv")
+	checkErr(err)
+
+	defer file.Close()
+
+	headers := []string{"ID", "Title", "Location"}
+
+	wErr := file.Write(headers)
+	checkErr(wErr)
+
+	done := make(chan bool)
+
+	for _, job := range jobs {
+		go func(job extractedJob) {
+			jwErr := file.Write([]string{"https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx=" + job.id, job.title, job.location})
+			checkErr(jwErr)
+			// fmt.Println("ah")
+			done <- true
+		}(job)
+	}
+
+	for i := 0; i < len(jobs); i++ {
+		<-done
+	}
+}
+
+// func writeJobs(jobs []extractedJob) {
+// 	file, err := os.Create("jobs.csv")
+// 	checkErr(err)
+
+// 	w := csv.NewWriter(file)
+// 	// Flush는 파일에 데이터를 입력하는 함수이다.
+// 	//defer은 함수가 끝나는 시점에 실행된다.
+// 	defer w.Flush()
+
+// 	headers := []string{"ID", "Title", "Location"}
+
+// 	wErr := w.Write(headers)
+// 	checkErr(wErr)
+
+// 	for _, job := range jobs {
+// 		jobSlice := []string{"https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx=" + job.id, job.title, job.location}
+// 		jwErr := w.Write(jobSlice)
+// 		checkErr(jwErr)
+// 	}
+// }
 
 func checkErr(err error) {
 	if err != nil {
